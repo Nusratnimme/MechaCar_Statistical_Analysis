2bc273c0d84e63bf74017860ad92b7649807545b
Nusrat Jahan <nusratnimme@gmai.com>
2022-04-02 17:50:44 -0400

    Updated RScript.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,9 +3,7 @@
 library(dplyr)
 
 MechaCar_mpg <- read.csv('./Resources/MechaCar_mpg.csv',stringsAsFactors = F)
-
 lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg)
-
 summary(lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg))
 
 # Additional Scatter plots for 'vehicle_length' and 'ground_clearance' against 'mpg'
@@ -15,12 +13,10 @@ plt + geom_point()
 plt <- ggplot(MechaCar_mpg,aes(x=ground_clearance,y=mpg))
 plt + geom_point()
 
-# Deloiverable 2: Create Visualizations for the Trip Analysis
+# Deliverable 2: Create Visualizations for the Trip Analysis
 
 suspension_coil <- read.csv('./Resources/Suspension_Coil.csv',stringsAsFactors = F)
-
 total_summary <- suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
-
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 
 # Deliverable 3: T-Tests on Suspension Coils
@@ -30,9 +26,7 @@ t.test(suspension_coil$PSI ,mu= 1500)
 
 # t.test for individual lots
 t.test(subset(suspension_coil, Manufacturing_Lot=="Lot1")$PSI, mu= 1500)
-
 t.test(subset(suspension_coil, Manufacturing_Lot=="Lot2")$PSI, mu= 1500)
-
 t.test(subset(suspension_coil, Manufacturing_Lot=="Lot3")$PSI, mu= 1500)
 
 
