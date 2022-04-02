99cb9225a797b06b6169eda1219ffbf3b2478109
Nusrat Jahan <nusratnimme@gmai.com>
2022-04-02 00:10:39 -0400

    Adding visualizations for the trip analysis.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,8 +1,11 @@
 # Deliverable 1: Linear Regression to Predict MPG
 
 library(dplyr)
+
 MechaCar_mpg <- read.csv('./Resources/MechaCar_mpg.csv',stringsAsFactors = F)
+
 lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg)
+
 summary(lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg))
 
 # Additional Scatter plots for 'vehicle_length' and 'ground_clearance' against 'mpg'
@@ -11,3 +14,16 @@ plt + geom_point()
 
 plt <- ggplot(MechaCar_mpg,aes(x=ground_clearance,y=mpg))
 plt + geom_point()
+
+# Deloiverable 2 : Create Visualizations for the Trip Analysis
+
+suspension_coil <- read.csv('./Resources/Suspension_Coil.csv',stringsAsFactors = F)
+
+total_summary <- suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+
+
+
+
